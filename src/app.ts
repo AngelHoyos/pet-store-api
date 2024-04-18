272f8ae4e31769cb746368a4b865c0f6c52a6cd7
hoyosbi11o <hoyosboli20rgmail.com>
2024-04-18 13:09:02 -0500

    Estructura de carpetas inicial, creacion de servidor y configuracion de envs

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-console.log("hola a todos");
+import { envs } from "./config/envs";
+import { Server } from "./presentation/server";
+
+
+(()=>{
+    main()
+})()
+
+async function main() {
+    console.log("main");
+    
+    new Server({
+        port:envs.PORT
+    }).start()
+}
