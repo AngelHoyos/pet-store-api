45b8c8f859d9cbafd80a4e846ba59bf6462b59f9
hoyosbi11o <hoyosboli20rgmail.com>
2024-04-30 00:04:52 -0500

    Implementación del RegisterDTO al modulo auth de la Api

---
+++ src/domain/entities/auth/user.entity.ts
@@ -0,0 +1,10 @@
+class UserEntity {
+    constructor(
+        public id: string,
+        public name: string,
+        public email: string,   
+        public password: string,
+        public role:[],
+        public img? : string,
+    ) {}
+}
