43f25bda912c75a4ffd87872615e858541d4a874
AngelHoyos <hoyosboli20rgmail.com>
2024-05-15 22:49:58 -0500

    conectar base de datos mongoDB

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,6 +1,9 @@
-import 'dotenv/config'
-import { get } from 'env-var'
+import "dotenv/config";
+import { get } from "env-var";
 
 export const envs = {
-PORT: get("PORT").required().asPortNumber(),
-}
+  PORT: get("PORT").required().asPortNumber(),
+
+  MONGO_URL: get("MONGO_URL").required().asString(), //1
+  MONGO_DB_NAME: get("MONGO_DB_NAME").required().asString(), //2
+};
